85b17728ea7377742721364a0f263569482e4b02
saddam-satria <ardhisaddam@gmail.com>
2022-04-21 10:53:50 +0700

    update use redcuer

--- src/pages/hooks/useReducerPages.js
+++ src/pages/hooks/useReducerPages.js
@@ -1,7 +1,66 @@
 import React from 'react';
 
-const UseReducerPage = () => {
-  return <div>UseReducerPage</div>;
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case 'PLUS':
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
-export default UseReducerPage;
+const initialCtx = {
+  counter: 0,
+  name: 'sarah',
+};
+
+const counterContext = React.createContext();
+
+const mainCtx = () => {
+  return (
+    <counterContext.Provider value={initialCtx}>
+      <UseReducerPage />
+    </counterContext.Provider>
+  );
+};
+
+console.log('render');
+
+const UseReducerPage = () => {
+  const initialState = React.useContext(counterContext);
+
+  const [state, dispatch] = React.useReducer(counterReducer, initialState);
+  const { counter, name } = state;
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <h1>{name}</h1>
+      <button
+        type="button"
+        onClick={() => {
+          dispatch({ type: 'PLUS' });
+        }}
+      >
+        +
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          dispatch({ type: 'CHANGE_NAME', payload: 'John' });
+        }}
+      >
+        change name
+      </button>
+    </div>
+  );
+};
+
+export default mainCtx;
